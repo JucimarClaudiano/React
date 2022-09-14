4ea6dcb94759c3a85282021eb8503f82f78d80dd
JucimarClaudiano <48931237+JucimarClaudiano@users.noreply.github.com>
2022-09-14 19:57:10 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+//importacao
+import Integration from "./Components/Integration";
+
+//folha de estilo
+//import "./App.css";
+import "./Style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Integrando conhecimento com ORIGAMED</h1>
+      <Integration />
+    </div>
+  );
+}
+
+export default App;
---
+++ Integration.js
@@ -0,0 +1,44 @@
+const Integration = () => {
+  return (
+    <div>
+      <header>
+        <h1> Comércio Eletrônico</h1>
+      </header>
+      <div class="grid">
+        <div class="item">
+          <h1>Camiseta Polo</h1>
+          <h2> R$ 39.90</h2>
+          <p> Camiseta de algodão 100% com detalhes nas mangas na cor azul</p>
+          <a class="compra" href="/">
+            comprar
+          </a>
+        </div>
+        <div class="item">
+          <h1>Calça jeans</h1>
+          <h2> R$ 39.90</h2>
+          <p> Camiseta de algodão 100% com detalhes nas mangas na cor azul</p>
+          <a class="compra" href="/">
+            comprar
+          </a>
+        </div>
+        <div class="item">
+          <h1>blusa de moleton</h1>
+          <h2> R$ 39.90</h2>
+          <p> Camiseta de algodão 100% com detalhes nas mangas na cor azul</p>
+          <a class="compra" href="/">
+            comprar
+          </a>
+        </div>
+        <div class="item">
+          <h1>bermuda tectel</h1>
+          <h2> R$ 39.90</h2>
+          <p> Camiseta de algodão 100% com detalhes nas mangas na cor azul</p>
+          <a class="compra" href="/">
+            comprar
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Integration;
